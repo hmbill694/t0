46fa93564168910da8f713a6fdd9845a0dd6e8d6
Harrison Billings <harry-mac@Harrisons-Laptop.local>
2025-04-01 15:24:26 -0700

    layout updated

--- lib/layouts/main-layout.tsx
+++ lib/layouts/main-layout.tsx
@@ -1,6 +1,7 @@
 import { Html } from "@elysiajs/html";
 import NavBar from "../ui/nav/nav";
 import type { Children } from "@kitajs/html";
+import DocumentHead from "../ui/document-head/document-head";
 
 export type MainLayoutProps = {
     children: Children
@@ -10,14 +11,7 @@ export type MainLayoutProps = {
 export default function MainLayout(props: MainLayoutProps) {
   return (
     <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="T0 Application Shell" />
-        <title>{props.title ?? "T0"}</title>
-        <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
-        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-      </head>
+      <DocumentHead />
       <body class="bg-gray-100 min-h-screen flex flex-col">
         <NavBar />
 
