acc40aa0c67fb98d256693af6b5cbc34df583d9e
Harrison Billings <harry-mac@Harrisons-Laptop.local>
2025-04-01 15:24:09 -0700

    service updated to talk to DB

--- lib/services/generated-documents.ts
+++ lib/services/generated-documents.ts
@@ -1,7 +1,44 @@
-import { readdir } from 'node:fs/promises';
-import { Result } from '../utils/result';
+import { Result } from "../utils/result";
+import { db } from "../db/client";
+import { pages } from "../db/schema";
+import { and, eq } from "drizzle-orm";
 
-export async function getFileNames(directoryPath: string): Promise<string[]> {
-    const readFileResult = await Result.fromAsync(() => readdir(directoryPath))
-    return readFileResult.getOrThrow()
+export async function getGeneratedPages() {
+  const res = await Result.fromAsync(() => db.select().from(pages));
+
+  return res.getOrThrow();
+}
+
+export async function saveDocument(doc: {
+  content: string;
+  name: string;
+  projectId: number;
+}) {
+  const res = await Result.fromAsync(() =>
+    db
+      .insert(pages)
+      .values([{ createdAt: new Date(), ...doc }])
+      .returning()
+  );
+
+  return res.map(ele => ele.at(0)).ifNull("Could not find the requested record").getOrThrow();
+}
+
+export async function getGeneratedDoc(query: {
+  docId: number;
+  projectId: number;
+}) {
+  const res = await Result.fromAsync(() =>
+    db
+      .select()
+      .from(pages)
+      .where(
+        and(eq(pages.id, query.docId), eq(pages.projectId, query.projectId))
+      )
+  );
+
+  return res
+    .map((ele) => ele.at(0))
+    .ifNull("Could not get the requested document")
+    .getOrThrow()!!;
 }
